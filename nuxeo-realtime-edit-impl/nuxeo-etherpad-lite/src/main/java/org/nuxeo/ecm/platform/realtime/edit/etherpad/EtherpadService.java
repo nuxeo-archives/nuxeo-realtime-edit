dc13b2b9617e4a2102a875571f4d68ecf6c51a48
Damien Metzler <dmetzler@gmail.com>
2013-10-07 16:51:35 +0200

    NXP-12756 Embed etherpad implementation

---
+++ nuxeo-realtime-edit-impl/nuxeo-etherpad-lite/src/main/java/org/nuxeo/ecm/platform/realtime/edit/etherpad/EtherpadService.java
@@ -0,0 +1,7 @@
+package org.nuxeo.ecm.platform.realtime.edit.etherpad;
+
+import org.nuxeo.ecm.platform.realtime.edit.RealtimeEditService;
+
+public interface EtherpadService extends RealtimeEditService {
+
+}
